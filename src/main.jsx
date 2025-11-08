d60eb12b70ac8a58a31cb4269a060b64ab513aa0
Zara Palevani <zpalevani@gmail.com>
2025-11-08 10:46:23 -0500

    Feat: Redesign UI with minimal, modern theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import ErrorBoundary from './components/ErrorBoundary'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Or whatever your main component is
+import './index.css'; // This is the new line you just added
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
