80b74c546c656255e280443268211c893897fa8d
Zara Palevani <zpalevani@gmail.com>
2025-11-08 19:13:01 -0500

    deploy build fix

--- src/components/TaskMenu.jsx
+++ src/components/TaskMenu.jsx
@@ -62,8 +62,10 @@ export function TaskMenu({
         >
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             {currentPriority === priority ? (
-              <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-              <polyline points="22 4 12 14.01 9 11.01"></polyline>
+              <>
+                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                <polyline points="22 4 12 14.01 9 11.01"></polyline>
+              </>
             ) : (
               <circle cx="12" cy="12" r="10"></circle>
             )}
